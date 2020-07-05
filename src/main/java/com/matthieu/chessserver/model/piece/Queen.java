2ee4e793b9c3b870bceb2ca444106e3aa9bda86e
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2020-07-05 12:15:46 +0200

    Implement Queen range

--- src/main/java/com/matthieu/chessserver/model/piece/Queen.java
+++ src/main/java/com/matthieu/chessserver/model/piece/Queen.java
@@ -1,9 +1,11 @@
 package com.matthieu.chessserver.model.piece;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.matthieu.chessserver.model.Board;
+import com.matthieu.chessserver.model.Letter;
 import com.matthieu.chessserver.model.entity.Coordinate;
 
 public class Queen extends Piece {
@@ -14,8 +16,73 @@ public class Queen extends Piece {
 
 	@Override
 	public List<Coordinate> getRangeFrom(Coordinate c, Board board) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Coordinate> possibleCoordinates = new ArrayList<>();
+		
+		// Right
+		Coordinate tmpCoord = new Coordinate(Letter.valueOf(c.getLetter().getValue() - 1), c.getDigit());
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(Letter.valueOf(tmpCoord.getLetter().getValue() - 1), tmpCoord.getDigit());
+		}
+		
+		// Left
+		tmpCoord = new Coordinate(Letter.valueOf(c.getLetter().getValue() + 1), c.getDigit());
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(Letter.valueOf(tmpCoord.getLetter().getValue() + 1), tmpCoord.getDigit());
+		}
+		
+		// Up
+		tmpCoord = new Coordinate(c.getLetter(), c.getDigit() + 1);
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(c.getLetter(), c.getDigit() + 1);
+		}
+		
+		// Down
+		tmpCoord = new Coordinate(c.getLetter(), c.getDigit() - 1);
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(c.getLetter(), c.getDigit() - 1);
+		}
+		
+		// Top Right
+		tmpCoord = new Coordinate(Letter.valueOf(c.getLetter().getValue() - 1), c.getDigit() + 1);
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(Letter.valueOf(tmpCoord.getLetter().getValue() - 1), tmpCoord.getDigit() + 1);
+		}
+		
+		// Top Left
+		tmpCoord = new Coordinate(Letter.valueOf(c.getLetter().getValue() + 1), c.getDigit() + 1);
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(Letter.valueOf(tmpCoord.getLetter().getValue() + 1), tmpCoord.getDigit() + 1);
+		}
+
+		// Bottom Right
+		tmpCoord = new Coordinate(Letter.valueOf(c.getLetter().getValue() - 1), c.getDigit() - 1);
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(Letter.valueOf(tmpCoord.getLetter().getValue() - 1), tmpCoord.getDigit() - 1);
+		}
+		
+		// Bottom Left
+		tmpCoord = new Coordinate(Letter.valueOf(c.getLetter().getValue() + 1), c.getDigit() - 1);
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(Letter.valueOf(tmpCoord.getLetter().getValue() + 1), tmpCoord.getDigit() - 1);
+		}
+				
+		return possibleCoordinates;
 	}
 
 }
