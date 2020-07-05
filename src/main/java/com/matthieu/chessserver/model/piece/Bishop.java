c5726b0a497ec6c2a2376211c3a2afde1c9c651b
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2020-07-05 12:12:15 +0200

    Implement Bishop range

--- src/main/java/com/matthieu/chessserver/model/piece/Bishop.java
+++ src/main/java/com/matthieu/chessserver/model/piece/Bishop.java
@@ -1,9 +1,11 @@
 package com.matthieu.chessserver.model.piece;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.matthieu.chessserver.model.Board;
+import com.matthieu.chessserver.model.Letter;
 import com.matthieu.chessserver.model.entity.Coordinate;
 
 public class Bishop extends Piece {
@@ -14,8 +16,41 @@ public class Bishop extends Piece {
 
 	@Override
 	public List<Coordinate> getRangeFrom(Coordinate c, Board board) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Coordinate> possibleCoordinates = new ArrayList<>();
+		
+		// Top Right
+		Coordinate tmpCoord = new Coordinate(Letter.valueOf(c.getLetter().getValue() - 1), c.getDigit() + 1);
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(Letter.valueOf(tmpCoord.getLetter().getValue() - 1), tmpCoord.getDigit() + 1);
+		}
+		
+		// Top Left
+		tmpCoord = new Coordinate(Letter.valueOf(c.getLetter().getValue() + 1), c.getDigit() + 1);
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(Letter.valueOf(tmpCoord.getLetter().getValue() + 1), tmpCoord.getDigit() + 1);
+		}
+
+		// Bottom Right
+		tmpCoord = new Coordinate(Letter.valueOf(c.getLetter().getValue() - 1), c.getDigit() - 1);
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(Letter.valueOf(tmpCoord.getLetter().getValue() - 1), tmpCoord.getDigit() - 1);
+		}
+		
+		// Bottom Left
+		tmpCoord = new Coordinate(Letter.valueOf(c.getLetter().getValue() + 1), c.getDigit() - 1);
+		while(!board.isOutOfBoard(tmpCoord) || board.getPiece(tmpCoord) == null) {
+			possibleCoordinates.add(tmpCoord);
+			
+			tmpCoord = new Coordinate(Letter.valueOf(tmpCoord.getLetter().getValue() + 1), tmpCoord.getDigit() - 1);
+		}
+		
+		return possibleCoordinates;
 	}
 
 }
