c47b7cdf99fccb6ba8cd5c820f600e6a69394203
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2020-07-05 19:14:37 +0200

    Implement king range

--- src/main/java/com/matthieu/chessserver/model/piece/King.java
+++ src/main/java/com/matthieu/chessserver/model/piece/King.java
@@ -1,9 +1,11 @@
 package com.matthieu.chessserver.model.piece;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.matthieu.chessserver.model.Board;
+import com.matthieu.chessserver.model.Letter;
 import com.matthieu.chessserver.model.entity.Coordinate;
 
 public class King extends Piece {
@@ -14,8 +16,19 @@ public class King extends Piece {
 
 	@Override
 	public List<Coordinate> getRangeFrom(Coordinate c, Board board) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Coordinate> possibleCoordinates = new ArrayList<>();
+		
+		for (int i = c.getLetter().getValue() - 1; i < c.getLetter().getValue() + 1; i++) {
+			for (int j = c.getDigit() - 1; j < c.getDigit() + 1; j++) {
+				Coordinate tmpCoord = new Coordinate(Letter.valueOf(i), j);
+				
+				if(!board.isOutOfBoard(tmpCoord)) {
+					possibleCoordinates.add(tmpCoord);
+				}
+			}
+		}
+		
+		return possibleCoordinates;
 	}
 
 }
