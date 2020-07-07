b441a4a469ec33c6a7fd226f2e3b825501b77d3f
lucas <lucas.mourado@buawei.com>
2020-07-07 13:26:33 +0200

    fixing Rook vector creation

--- src/main/java/com/matthieu/chessserver/model/piece/Rook.java
+++ src/main/java/com/matthieu/chessserver/model/piece/Rook.java
@@ -9,22 +9,18 @@ import com.matthieu.chessserver.model.Vector;
 
 public class Rook extends Piece {
 	
-	private List<Vector> range;
-
 	public Rook(Color color) {
 		super(PieceType.ROOK, color);
-		
-		this.range = new LinkedList<>();
-		
-		this.range.add(new Vector(-1, 0, Board.BOARD_SIZE));
-		this.range.add(new Vector(1, 0, Board.BOARD_SIZE));
-		this.range.add(new Vector(0, -1, Board.BOARD_SIZE));
-		this.range.add(new Vector(0, 1, Board.BOARD_SIZE));
 	}
 
 	@Override
 	protected List<Vector> getRange() {
-		return this.range;
+		List<Vector> range = new LinkedList<>();
+		range.add(new Vector(-1, 0, Board.BOARD_SIZE));
+		range.add(new Vector(1, 0, Board.BOARD_SIZE));
+		range.add(new Vector(0, -1, Board.BOARD_SIZE));
+		range.add(new Vector(0, 1, Board.BOARD_SIZE));
+		return range;
 	}
 
 
