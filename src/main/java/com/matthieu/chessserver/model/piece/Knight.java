f8fadeed9c1f5e4a88e3b8b75a89f45d4e6f4288
lucas <lucas.mourado@buawei.com>
2020-07-07 13:26:19 +0200

    fixing Knight range vector

--- src/main/java/com/matthieu/chessserver/model/piece/Knight.java
+++ src/main/java/com/matthieu/chessserver/model/piece/Knight.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.util.LinkedList;
 import java.util.List;
 
-import com.matthieu.chessserver.model.Board;
 import com.matthieu.chessserver.model.Vector;
 
 public class Knight extends Piece {
@@ -17,15 +16,15 @@ public class Knight extends Piece {
 	protected List<Vector> getRange() {
 		List<Vector> range = new LinkedList<>();
 		
-		range.add(new Vector(-2, -1, Board.BOARD_SIZE));
-		range.add(new Vector(2, -1, Board.BOARD_SIZE));
-		range.add(new Vector(-2, 1, Board.BOARD_SIZE));
-		range.add(new Vector(2, 1, Board.BOARD_SIZE));
+		range.add(new Vector(-2, -1, 1));
+		range.add(new Vector(2, -1, 1));
+		range.add(new Vector(-2, 1, 1));
+		range.add(new Vector(2, 1, 1));
 
-		range.add(new Vector(-1, -2, Board.BOARD_SIZE));
-		range.add(new Vector(-1, 2, Board.BOARD_SIZE));
-		range.add(new Vector(1, -2, Board.BOARD_SIZE));
-		range.add(new Vector(1, 2, Board.BOARD_SIZE));
+		range.add(new Vector(-1, -2, 1));
+		range.add(new Vector(-1, 2, 1));
+		range.add(new Vector(1, -2, 1));
+		range.add(new Vector(1, 2, 1));
 		
 		return range;
 	}
